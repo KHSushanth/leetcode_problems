bdd9283581f519ee796e994f5f934b81ef84afdf
KHSushanth <sushanthkh836@gmail.com>
2026-09-13 23:12:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (83.56%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -3,9 +3,7 @@ class Solution {
         s=s.trim();
         int i=s.length()-1;
         int count=0;
-        while (i >= 0 && s.charAt(i) == ' ') {
-            i--;
-        }
+        
         while(i>=0 && s.charAt(i) !=' '){
             count++;
             i--;
